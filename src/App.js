d0a5b47121ec984d2870d30079b9104cba156318
NhiPH <nhiph@splus-software.com.vn>
2021-03-31 22:11:47 +0700

    ssds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, BrowserRouter as router, Route, Switch} from 'react-router-dom';
+import AddTodo from './components/AddTodo';
+import ListTodo from './components/ListTodo';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/addTodo" component={AddTodo}/>
+      <Route exact path="/listTodo" component={ListTodo} />
+      <Route exact path="/listTodo/:id/edit" component={AddTodo}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
